e4f3d91bf560a81474cfb5ee8ea23f3f1c0cfad1
davidherguetasoto <47791138+davidherguetasoto@users.noreply.github.com>
2019-04-30 20:44:07 +0200

    Introducida función 'TonoAlarma'
    Añadida función 'TonoAlarma' que al llamarla reproduce el sonido de una alarma en un zumbador.

--- Alarma arduino/programa_arduino/programa_arduino.ino
+++ Alarma arduino/programa_arduino/programa_arduino.ino
@@ -1,3 +1,6 @@
+
+void TonoAlarma(int); //Reproduce el sonido de la alarma, debes indicarle el pin del zumbador
+
 void setup()
 {
 
@@ -7,4 +10,19 @@ void loop()
 {
 
 
+}
+
+void TonoAlarma(int Zumbador)
+{
+  int i;
+  float sinVal;
+  int toneVal;
+  
+   for(i=0;i<180;i++)
+  {
+    sinVal=(sin(i*(3.1412/180)));
+    toneVal=2000+(int)(sinVal*1000);
+    tone(Zumbador,toneVal);
+    delay(2);
+  }
 }
