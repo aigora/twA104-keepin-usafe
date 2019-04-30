b7111c6852c308d4cee135792f2afaa63761db56
davidherguetasoto <47791138+davidherguetasoto@users.noreply.github.com>
2019-04-30 21:00:31 +0200

    Añadida función 'MedirDistancia'
    Añadida función 'MedirDistancia' que devuelve la distancia a la que se encuentra el objeto del sensor de ultrasonidos

--- Alarma arduino/programa_arduino/programa_arduino.ino
+++ Alarma arduino/programa_arduino/programa_arduino.ino
@@ -1,5 +1,6 @@
-
+//Prototipo funciones
 void TonoAlarma(int); //Reproduce el sonido de la alarma, debes indicarle el pin del zumbador
+int MedirDistancia(int); //Mide la distancia desde el sensor ultrasonido, debes indicarle el pin de Echo
 
 void setup()
 {
@@ -12,6 +13,8 @@ void loop()
 
 }
 
+//Funciones
+
 void TonoAlarma(int Zumbador)
 {
   int i;
@@ -20,9 +23,20 @@ void TonoAlarma(int Zumbador)
   
    for(i=0;i<180;i++)
   {
-    sinVal=(sin(i*(3.1412/180)));
+    sinVal=(sin(i*(3.1412/180))); //Señal senoidal. Solo valores positivos
     toneVal=2000+(int)(sinVal*1000);
-    tone(Zumbador,toneVal);
+    tone(Zumbador,toneVal); //Tono zumbador
     delay(2);
   }
+}
+
+int MedirDistancia(int Echo)
+{
+  long duracion;
+  long distancia;
+
+  duracion=((pulseIn(Echo,HIGH))/2); //La señal sonora va y vuelve, por eso se divide por 2 
+  distancia=duracion/29.2; //Velocidad del sonido= 1cm/29.2 microsegundos
+
+  return distancia;
 }
