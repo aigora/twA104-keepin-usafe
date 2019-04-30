46529a4b7051592bb6ac9f59a8274df1baf939c5
davidherguetasoto <47791138+davidherguetasoto@users.noreply.github.com>
2019-04-30 18:40:18 +0200

    Subida de la carpeta del proyecto
    Carpeta del proyecto de informática

---
+++ Alarma arduino/Programa_alarma.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+//Biblioteca comunicación serial arduino
+#include"SerialPort.h"
+
+#define MAX_DATA_LENGTH 255
+
+// Funciones prototipo
+
+
+void main()
+{
+	
+}
