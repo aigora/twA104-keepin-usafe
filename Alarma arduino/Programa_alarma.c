d12b88bffcd47c3ec2df8b70817edc365706fb50
davidherguetasoto <47791138+davidherguetasoto@users.noreply.github.com>
2019-05-02 23:44:11 +0200

    Añadida función 'Password'
    Añadida función 'Password'. Se utiliza para introducir una contraseña con asignación dinámica. Devuelve el puntero de la contraseña

--- Alarma arduino/Programa_alarma.c
+++ Alarma arduino/Programa_alarma.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<malloc.h>
 //Biblioteca comunicación serial arduino
 #include"SerialPort.h"
 
+#define length 16 //Longitud máxima de la contraseña
 #define MAX_DATA_LENGTH 255
 
 //Funciones para comunicacion serial
@@ -14,11 +16,12 @@
 	int writeSerialPort(SerialPort *PuertoSerie, char *buffer, unsigned int buf_size);
 
 // Funciones prototipo
-
+	char* Password(); //Función para introducir contraseña con asignación dinámica
 
 void main()
 {
 	printf("			SISTEMA DE ALARMAS KEEP'N YOU SAFE\n\n");
+	
 
 	//Arduino SerialPort object
 	SerialPort *arduino;
@@ -34,6 +37,7 @@ void main()
 	autoConnect(arduino, incomingData);
 
 }
+
 
 void autoConnect(SerialPort *arduino, char *incomingData)
 {
@@ -65,6 +69,95 @@ void autoConnect(SerialPort *arduino, char *incomingData)
 	}
 }
 
+char* Password()
+{
+	char *pass, c = 0;
+	int i;
+	//Asignacion de memoria para los caracteres y '\0'
+	pass = (char*)malloc((length)+1);
+	if (pass == NULL)
+	{
+		printf("No hay memoria disponible\n");
+		system("PAUSE");
+		exit(1);
+	}
+
+	//Compobacion de que como mucho tiene los caracteres que hemos indicado. Cada vez que escribes un caracter, imprime *
+	do
+	{
+		i = 0;
+
+		while (c != 13) //Si no presionamos intro
+		{
+			if (i < (length + 1)) //Si la contraseña cabe en la cadena, se va introduciendo
+			{
+				c = _getch();
+
+				if (i == 0 && c == 8)
+					continue;
+
+				if (c > 31 && c < 126)
+				{
+					pass[i] = c;
+					printf("*");
+				}
+
+				else if (c == 8) //Pulsa retroceso
+				{
+					printf("\b \b"); //Mueve cursor y borra anterior caracter
+					c = NULL;
+					i = i - 2;
+				}
+				else if (c != 13) //Eliminamos teclas especiales
+				{
+					_getch();
+					continue;
+				}
+			}
+			else //Si no entra siemplemente se imprimen los *, luego te mandara repetir contraseña
+			{
+				c = _getch();
+				if (c != 8 && c != 13)
+				{
+					printf("*");
+				}
+
+				if (c == 8)
+				{
+					printf("\b \b");
+					c = NULL;
+					i = i - 2;
+				}
+				else if (c != 13)
+				{
+					_getch();
+					continue;
+				}
+			}
+			if (c == 13) //Pulsa intro
+			{
+				if (i < ((length)+1))
+					pass[i] = '\0';
+				else
+				{
+					pass[(length)+1] = '\0';
+				}
+				i--;
+				printf("\n");
+			}
+			i++;
+		}
+		if (i > length)
+			printf("Error de longitud, introducela de nuevo: ");
+		if (i == 0)
+			printf("La clave no puede estar en blanco, introducela de nuevo: ");
+		c = 0;
+	} while (i > length || i < 1);
+
+	//Reasignacion de memoria para optimizar
+	pass = (char*)realloc(pass, i * sizeof(char) + 1);
+	return pass; //Devuelve el puntero de la contraseña
+}
 
 
 //Funciones para la comunicacion serial
