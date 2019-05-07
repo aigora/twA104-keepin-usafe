8d0464d0d9235261f91c0114e7eb3b2108fa40a1
davidherguetasoto <47791138+davidherguetasoto@users.noreply.github.com>
2019-05-07 19:18:49 +0200

    Cambios menores

--- Alarma arduino/Programa_alarma.c
+++ Alarma arduino/Programa_alarma.c
@@ -7,7 +7,7 @@
 //Biblioteca comunicación serial arduino
 #include"SerialPort.h"
 
-#define length 16 //Longitud máxima de la contraseña de la alarma
+#define length 16 //Longitud máxima permitida en la clave de seguridad de la alarma
 #define MAX_DATA_LENGTH 255
 
 //Funciones para comunicacion serial
@@ -218,7 +218,7 @@ char* DefinePass(int flag)
 		system("CLS");
 		return pass1;
 	}
-	//Si ya existia,  la funcion devuelve la contraseña
+	//Si ya existia una contraseña creada,  la funcion la devuelve
 	else
 		return pass1;
 }
@@ -256,7 +256,7 @@ char* Password()
 					printf("*");
 				}
 
-				else if (c == 8) //Pulsa retroceso
+				else if (c == 8) //Si pulsa retroceso
 				{
 					printf("\b \b"); //Mueve cursor y borra anterior caracter
 					c = NULL;
@@ -288,7 +288,7 @@ char* Password()
 					continue;
 				}
 			}
-			if (c == 13) //Pulsa intro
+			if (c == 13) //Si pulsa intro
 			{
 				if (i < ((length)+1))
 					pass[i] = '\0';
@@ -315,107 +315,107 @@ char* Password()
 
 
 //Funciones para la comunicacion serial
-void Crear_Conexion(SerialPort *PuertoSerie, char *portName)
-{
-
-	PuertoSerie->connected = 0;
-	PuertoSerie->portName = portName;
-	PuertoSerie->handler = CreateFileA((portName),
-		GENERIC_READ | GENERIC_WRITE,
-		0,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
-	if (PuertoSerie->handler == INVALID_HANDLE_VALUE)
+	void Crear_Conexion(SerialPort *PuertoSerie, char *portName)
 	{
-		if (GetLastError() == ERROR_FILE_NOT_FOUND)
-		{
-			printf("ERROR: Handle was not attached. Reason: %s not available\n", portName);
-		}
-		else
-		{
-			printf("ERROR!!!");
-		}
-	}
-	else
-	{
-		DCB dcbSerialParameters = { 0 };
 
-		if (!GetCommState(PuertoSerie->handler, &dcbSerialParameters))
+		PuertoSerie->connected = 0;
+		PuertoSerie->portName = portName;
+		PuertoSerie->handler = CreateFileA((portName),
+			GENERIC_READ | GENERIC_WRITE,
+			0,
+			NULL,
+			OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL,
+			NULL);
+		if (PuertoSerie->handler == INVALID_HANDLE_VALUE)
 		{
-			printf("failed to get current serial parameters");
-		}
-		else
-		{
-			dcbSerialParameters.BaudRate = CBR_9600;
-			dcbSerialParameters.ByteSize = 8;
-			dcbSerialParameters.StopBits = ONESTOPBIT;
-			dcbSerialParameters.Parity = NOPARITY;
-			dcbSerialParameters.fDtrControl = DTR_CONTROL_ENABLE;
-
-			if (!SetCommState(PuertoSerie->handler, &dcbSerialParameters))
+			if (GetLastError() == ERROR_FILE_NOT_FOUND)
 			{
-				printf("ALERT: could not set Serial port parameters\n");
+				printf("ERROR: Handle was not attached. Reason: %s not available\n", portName);
 			}
 			else
 			{
-				PuertoSerie->connected = 1;
-				PurgeComm(PuertoSerie->handler, PURGE_RXCLEAR | PURGE_TXCLEAR);
-				Sleep(ARDUINO_WAIT_TIME);
+				printf("ERROR!!!");
 			}
 		}
-	}
-	return;
-}
-
-void CerrarConexion(SerialPort * PuertoSerie)
-{
-	if (PuertoSerie->connected)
-	{
-		PuertoSerie->connected = 0;
-		CloseHandle(PuertoSerie->handler);
-	}
-}
-
-int readSerialPort(SerialPort * PuertoSerie, char *buffer, unsigned int buf_size)
-{
-	DWORD bytesRead;
-	unsigned int toRead = 0;
-
-	ClearCommError(PuertoSerie->handler, &PuertoSerie->errors, &PuertoSerie->status);
-
-	if (PuertoSerie->status.cbInQue > 0)
-	{
-		if (PuertoSerie->status.cbInQue > buf_size)
+		else
 		{
-			toRead = buf_size;
+			DCB dcbSerialParameters = { 0 };
+
+			if (!GetCommState(PuertoSerie->handler, &dcbSerialParameters))
+			{
+				printf("failed to get current serial parameters");
+			}
+			else
+			{
+				dcbSerialParameters.BaudRate = CBR_9600;
+				dcbSerialParameters.ByteSize = 8;
+				dcbSerialParameters.StopBits = ONESTOPBIT;
+				dcbSerialParameters.Parity = NOPARITY;
+				dcbSerialParameters.fDtrControl = DTR_CONTROL_ENABLE;
+
+				if (!SetCommState(PuertoSerie->handler, &dcbSerialParameters))
+				{
+					printf("ALERT: could not set Serial port parameters\n");
+				}
+				else
+				{
+					PuertoSerie->connected = 1;
+					PurgeComm(PuertoSerie->handler, PURGE_RXCLEAR | PURGE_TXCLEAR);
+					Sleep(ARDUINO_WAIT_TIME);
+				}
+			}
 		}
-		else toRead = PuertoSerie->status.cbInQue;
+		return;
 	}
 
-	memset(buffer, 0, buf_size);
-
-	if (ReadFile(PuertoSerie->handler, buffer, toRead, &bytesRead, NULL)) return bytesRead;
-
-	return 0;
-}
-
-int writeSerialPort(SerialPort *PuertoSerie, char *buffer, unsigned int buf_size)
-{
-	DWORD bytesSend;
-
-	if (!WriteFile(PuertoSerie->handler, (void*)buffer, buf_size, &bytesSend, 0))
+	void CerrarConexion(SerialPort * PuertoSerie)
 	{
+		if (PuertoSerie->connected)
+		{
+			PuertoSerie->connected = 0;
+			CloseHandle(PuertoSerie->handler);
+		}
+	}
+
+	int readSerialPort(SerialPort * PuertoSerie, char *buffer, unsigned int buf_size)
+	{
+		DWORD bytesRead;
+		unsigned int toRead = 0;
+
 		ClearCommError(PuertoSerie->handler, &PuertoSerie->errors, &PuertoSerie->status);
+
+		if (PuertoSerie->status.cbInQue > 0)
+		{
+			if (PuertoSerie->status.cbInQue > buf_size)
+			{
+				toRead = buf_size;
+			}
+			else toRead = PuertoSerie->status.cbInQue;
+		}
+
+		memset(buffer, 0, buf_size);
+
+		if (ReadFile(PuertoSerie->handler, buffer, toRead, &bytesRead, NULL)) return bytesRead;
+
 		return 0;
 	}
-	else return 1;
-}
 
-int isConnected(SerialPort *PuertoSerie)
-{
-	if (!ClearCommError(PuertoSerie->handler, &PuertoSerie->errors, &PuertoSerie->status))
-		PuertoSerie->connected = 0;
-	return PuertoSerie->connected;
-}
+	int writeSerialPort(SerialPort *PuertoSerie, char *buffer, unsigned int buf_size)
+	{
+		DWORD bytesSend;
+
+		if (!WriteFile(PuertoSerie->handler, (void*)buffer, buf_size, &bytesSend, 0))
+		{
+			ClearCommError(PuertoSerie->handler, &PuertoSerie->errors, &PuertoSerie->status);
+			return 0;
+		}
+		else return 1;
+	}
+
+	int isConnected(SerialPort *PuertoSerie)
+	{
+		if (!ClearCommError(PuertoSerie->handler, &PuertoSerie->errors, &PuertoSerie->status))
+			PuertoSerie->connected = 0;
+		return PuertoSerie->connected;
+	}
